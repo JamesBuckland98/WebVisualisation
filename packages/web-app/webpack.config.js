de3cb539391cf8b9f25097bfbad700f3d00538ac
James Buckland <jamesbuckland98@gmai.com>
2020-03-03 16:48:13 +0000

    reducing compiling time
    Former-commit-id: a49a79f0a143a68e4dd9fa9eee12a31b3a70ff38

--- packages/web-app/webpack.config.js
+++ packages/web-app/webpack.config.js
@@ -1,12 +1,10 @@
-{
-    [
-        /(xlsx|canvg)/,
-        function(context, request, callback) {
-            if (/(pdfmake)/.test(request)) {
-                return callback(null, `commonjs ${request}`);
-            }
-
-            callback();
-        },
-    ];
-}
+module.exports = {
+    // used to disable external files on for amchart4 library
+    // https://www.amcharts.com/docs/v4/getting-started/integrations/using-webpack/#Large_file_sizes
+    externals(context, request, callback) {
+        if (/xlsx|canvg|pdfmake/.test(request)) {
+            return callback(null, `commonjs ${request}`);
+        }
+        callback();
+    },
+};
